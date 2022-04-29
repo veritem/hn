1d1d7e2e20682ef4f2c859d44753a7f39faa4d0b
Makuza Mugabo Verite <mugaboverite@gmail.com>
2022-04-29 10:19:25 +0200

    feat: updated

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,20 @@
 <script setup>
-     const  {data} = await useFetch("https://api.hnpwa.com/v0/newest/1.json")
+const { data } = await useFetch("https://api.hnpwa.com/v0/newest/1.json")
 </script>
 
 <template>
-  <h1 class="text-orange-500 font-sans">Hacker news</h1>
+  <section>
 
-  <div v-for="article in data" :key="article.id">
-   <a :href="article.url" target="_blank">
-     {{article.title}} <span>{{article.time_ago}}</span>
-   </a>
-  </div>
+    <Head>
+      <Title>Hn</Title>
+      <Meta name="description" content="Hack news" />
+    </Head>
+    <h1 class="text-orange-500 font-sans">Hacker news</h1>
+
+    <div v-for="article in data" :key="article.id">
+      <a :href="article.url" target="_blank">
+        {{ article.title }} <span>{{ article.time_ago }}</span>
+      </a>
+    </div>
+  </section>
 </template>
