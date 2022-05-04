15271f6b1e77c20a903ee3109c0e2924f88fc30f
Makuza Mugabo Verite <mugaboverite@gmail.com>
2022-05-04 11:21:34 +0200

    feat: fiixng nuxt on vercel

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,6 @@ import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  target: "static",
   buildModules: ["@unocss/nuxt"],
   unocss: {
     uno: true,
