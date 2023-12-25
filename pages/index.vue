638133b6bba875f1218a972c3e627da182fc8ccc
Verite Mugabo <mugaboverite@gmail.com>
2023-12-24 19:06:06 -0500

    chore(cleanup): remove un used code

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,24 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-import { vInfiniteScroll } from '@vueuse/components'
 import { Article } from '~/components/ArticleList.vue';
 import ArticleComponent from '~/components/Article.vue';
 
 
 const counter = ref(1)
-const items = ref<Article[]>([])
 
-const { data, error } = await useFetch<Article[]>(`https://api.hnpwa.com/v0/news/${counter.value}.json`);
+const { data } = await useFetch<Article[]>(`https://api.hnpwa.com/v0/news/${counter.value}.json`);
 
-function onLoadMore() {
-	counter.value += 1
-}
 
 </script>
 
 <template>
-	<div v-infinite-scroll="[onLoadMore, { distance: 1000 }]">
+	<div>
 		<div v-for="(item, index) in  data ">
 			<ArticleComponent :article="item" :key="index" />
+		</div>
+
+		<div v-if="!data">
+			Loading...
 		</div>
 	</div>
 </template>
